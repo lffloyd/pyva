9dbbdef5b0270d48efe28aa6745f18d7ed71e987
holandafelipeuff <felipeholanda@id.uff.br>
2019-10-21 02:34:42 -0300

    Implementacao da retirada de comentarios do codigo fonte java

--- main.py
+++ main.py
@@ -2,6 +2,39 @@ from src.scanner import generateScanner
 
 miniJavaFile = open("miniJava.java","r")
 
-fileContent = miniJavaFile.read()
+fileContent = ""
+
+multiLineComment = False
+
+line = miniJavaFile.readline()
+
+while(line):
+    if(multiLineComment):
+
+        if(line.find("*/") != -1):
+            multiLineComment = False
+            auxLine = (line.split("*/"))
+            line = auxLine[-1]
+            fileContent = fileContent + line    
+        
+    else:
+
+        if(line.find("/*") != -1):
+            multiLineComment = True
+            auxLine = (line.split("/*"))
+            line = auxLine[0] + "\n"
+            fileContent = fileContent + line    
+        else:
+            if(line.find("//") != -1):
+                auxLine = (line.split("//"))
+                line = auxLine[0] + "\n"
+                fileContent = fileContent + line    
+            else:
+                fileContent = fileContent + line    
+    line = miniJavaFile.readline()
+    
+print(fileContent)
 
 generateScanner(fileContent)
+
+miniJavaFile.close()
