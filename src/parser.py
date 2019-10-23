13c86f5cb1b1912314b21aca933c02cdc1503021
Luiz Felipe Matos <lfmatosmelo@id.uff.br>
2019-10-22 22:55:27 -0300

    WIP
    - testa parser;

--- src/parser.py
+++ src/parser.py
@@ -68,3 +68,83 @@
 # pexp -> ID | THIS | NEW ID LPAREN RPREN | LPAREN exp RPAREN | pexp ID (verificar pois na gramática dele é ".id" e a nossa é "id") | pexp ID (verificar pois na gramática dele é ".id" e a nossa é "id") LPAREN pode_ou_nao(exps) RPAREN
 # exps -> exp RKEY COLON exp RKEY <--- verificar se aqui é COLON mesmo
 
+# Yacc example
+
+import ply.yacc as yacc
+
+# Get the token map from the lexer.  This is required.
+from src.scanner import tokens
+
+
+def p_expression_plus(p):
+    'expression : expression PLUS term'
+    p[0] = p[1] + p[3]
+
+
+def p_expression_minus(p):
+    'expression : expression MINUS term'
+    p[0] = p[1] - p[3]
+
+
+def p_expression_term(p):
+    'expression : term'
+    p[0] = p[1]
+
+
+def p_term_times(p):
+    'term : term TIMES factor'
+    p[0] = p[1] * p[3]
+
+
+def p_term_div(p):
+    'term : term DIVIDE factor'
+    p[0] = p[1] / p[3]
+
+
+def p_term_factor(p):
+    'term : factor'
+    p[0] = p[1]
+
+
+def p_factor_num(p):
+    'factor : NUMBER'
+    p[0] = p[1]
+
+
+def p_factor_expr(p):
+    'factor : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
+
+
+# Build the parser
+parser = yacc.yacc()
+
+while True:
+    try:
+        s = """class Factorial {
+    public static void main(String[] a) {
+        System.out.println(new Fac().ComputeFac(10));
+    }
+}
+
+class Fac {
+    public int ComputeFac(int num) {
+        int num_aux;
+        int statica;
+        if (num < 1)
+            num_aux = 1;
+        else
+            num_aux = num * (this.ComputeFac(num - 1));
+        return num_aux;
+    }
+}"""
+    except EOFError:
+        break
+    if not s: continue
+    result = parser.parse(s)
+    print(result)
