912d774f12cd53c466555db36182e0ea34fe8d54
holandafelipeuff <felipeholanda@id.uff.br>
2019-10-24 01:30:23 -0300

    FINALMENTE ENTENDI ESSE NODE

--- src/parser.py
+++ src/parser.py
@@ -6,6 +6,52 @@ import ply.yacc as yacc
 from src.scanner import tokens
 
 
+class Node:
+    def __init__(self,type,children=None,leaf=None):
+        self.type = type
+        if children:
+            self.children = children
+        else:
+            self.children = [ ]
+        self.leaf = leaf
+
+def p_prog(p):
+    'prog : main conj_classes'
+    non_terminals = [p[1], p[2]]
+
+    p[0] = Node(type = "prog", children = non_terminals)
+
+def p_main(p):
+    'main : CLASS ID LKEY PUBLIC STATIC VOID MAIN LPAREN STRING LBRACKET RBRACKET ID RPAREN LKEY cmd1 RKEY RKEY'
+    non_terminals = [p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9], p[10], p[11], p[12], p[13], p[14], p[16]]
+    tokens = [p[15]]
+
+    p[0] = Node(type = "main", children = non_terminals, leaf = tokens)
+
+def p_conj_classes(p):
+    ''' 
+    conj_classes : EPISILON
+    | conj_classes classe
+    '''
+
+    if(len(p) > 2):
+        non_terminals = [p[1], p[2]]
+        p[0] = Node(type = "conj_classes", children = non_terminals)
+    else:
+        #caso seja a transição episilon
+        pass
+
+def p_classe(p):
+    'classe : CLASS ID extension LKEY conj_var conj_metodos RKEY'
+    children = [p[3], p[5], p[6]]
+    leaf = [p[1], p[2], p[4], p[7]]
+
+    p[0] = Node(type = "classe", children = non_terminals, leaf = tokens)
+
+
+
+'''
+
 def p_prog(p):
     'prog : main LKEY classe RKEY'
     p[0] = p[3]
@@ -42,6 +88,8 @@ def p_classe_no_var_no_metodo(p):
 def p_var(p):
     'var : tipo ID SEMICOLON'
     p[0] = p[1]
+
+'''
 
 
 # Error rule for syntax errors
