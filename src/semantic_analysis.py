76f9c90381781faf614eb718e810fc720cf5f261
Patricia <patriciaraposo@id.uff.br>
2019-12-08 18:41:53 -0300

    Limpando codigo

--- src/semantic_analysis.py
+++ src/semantic_analysis.py
@@ -15,15 +15,14 @@ def analiseSemantica(info, sum = 0, mult = 1):
     global symbolT
     if(info != None and type(info) is ASTNode):
         if (info.type == "mexp" ):
-            sum = 0
             if(info.val):
                 return ReturnType.total
-            elif len(info.children) > 1:                            
+            elif len(info.children) > 1:
+                #Se um dos termos é constante a função propaga a constante para dentro do outro termo
+                #  que apos ser analisado retorna se essa constante foi usada e pode ser descartada                          
                 if(info.children[1] == "*"  and info.children[2].val != None):                     
                     mult = mult * info.children[2].val
-                    
                     resp = analiseSemantica(info.children[0], mult = mult)
-                    print(info.children[0].val)
                     if resp == ReturnType.partialMult:
                         info.set(
                             type = info.children[0].type,
@@ -35,10 +34,7 @@ def analiseSemantica(info, sum = 0, mult = 1):
                     return ReturnType.partialMult
                 elif(info.children[1] == "*"  and info.children[0].val != None):                              
                     mult = mult * info.children[0].val
-                    #print(info.children[2])
                     resp = analiseSemantica(info.children[2], mult = mult)
-                    print(info.children[2].type)
-                    print(info.children[2].val)
                     if resp == ReturnType.partialMult:
                         info.set(
                             type = info.children[2].type,
@@ -49,6 +45,7 @@ def analiseSemantica(info, sum = 0, mult = 1):
                         setValToAll(info.children[0], mult)
                     return ReturnType.partialMult
                 else:
+                    #Necessario pois caso não exista constante nos termos eles ainda precisam ser analisados semanticamente
                     resp = analiseSemantica(info.children[0], mult = mult)
                     if resp == ReturnType.partialMult:
                         analiseSemantica(info.children[2])
@@ -57,18 +54,17 @@ def analiseSemantica(info, sum = 0, mult = 1):
                         return analiseSemantica(info.children[2], mult = mult)
 
         elif (info.type == "aexp"):
-            mult = 1
             if(info.val):
                 return ReturnType.total
-            elif len(info.children) > 1:                            
+            elif len(info.children) > 1:
+                #Se um dos termos é constante a função propaga a constante para dentro do outro termo
+                #  que apos ser analisado retorna se essa constante foi usada e pode ser descartada                                
                 if(info.children[1] and info.children[2].val != None):
-                    #print(info.children[0])
                     if(info.children[1] == "+" ):
                         sum = sum + info.children[2].val
                     if(info.children[1] == "-" ):
                         sum = sum - info.children[2].val
                     resp = analiseSemantica(info.children[0], sum)
-                    print(info.children[0].val)
                     if resp == ReturnType.partialSum:
                         info.set(
                             type = info.children[0].type,
@@ -79,14 +75,11 @@ def analiseSemantica(info, sum = 0, mult = 1):
                         setValToAll(info.children[2], sum)
                     return ReturnType.partialSum
                 elif(info.children[1] and info.children[0].val != None):
-                    #print(info.children[2])
                     if(info.children[1] == "+" ):
                         sum = sum + info.children[0].val
                     if(info.children[1] == "-" ):
                         sum = info.children[0].val - sum
                     resp = analiseSemantica(info.children[2], sum)
-                    print(info.children[2].type)
-                    print(info.children[2].val)
                     if resp == ReturnType.partialSum:
                         info.set(
                             type = info.children[2].type,
@@ -97,6 +90,7 @@ def analiseSemantica(info, sum = 0, mult = 1):
                         setValToAll(info.children[0], sum)
                     return ReturnType.partialSum
                 else:
+                    #Necessario pois caso não exista constante nos termos eles ainda precisam ser analisados semanticamente
                     resp = analiseSemantica(info.children[0], sum = sum)
                     if resp == ReturnType.partialSum:
                         analiseSemantica(info.children[2])
@@ -107,6 +101,7 @@ def analiseSemantica(info, sum = 0, mult = 1):
             for item in info.children:
                 if type(item) is ASTNode:  
                     if (item.type == "class"):
+                        #Limpando a tabela de simbolos
                         symbolT = SymbolTable()
                         analiseSemantica(item)
                     elif (item.type == "metodo"):
@@ -155,14 +150,14 @@ def analiseSemantica(info, sum = 0, mult = 1):
                                 symbolT.insert_entry(
                                     item.children[item.toTable['pos']], novo)
                         analiseSemantica(item)
+                    #Propagando valor para nos superiores na arvore caso so tenha um filho e a analise dele tenha atribuido valor
                     if len(info.children) == 1 and item.val:
                         info.val = item.val
                 elif (item == "{"):
                     symbolT.insert_scope(Scope(table = symbolT.scopes[symbolT.current_scope_level].table))
                 elif (item == "}"):
-                    #print(symbolT.scopes[symbolT.current_scope_level].table)
                     symbolT.remove()
-                    #print(symbolT.scopes[symbolT.current_scope_level].table)
+
 def setValToAll(info, val):
     info.set(val = val)
     index = 0
